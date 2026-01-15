f0adce3989a380a7aadf4b1ae992e270177b61b1
Alejandro R <alexestudio86@hotmail.com>
2026-01-14 23:46:58 -0600

    tab system started in articles footer

--- src/components/projects/projectsIcons.js
+++ src/components/projects/projectsIcons.js
@@ -1,9 +1,13 @@
+import IconAstro from "@src/assets/icons/IconAstro.svglApp.astro";
+import W3Schools from "@src/assets/icons/W3Schools.astro";
 import Vite from "@src/assets/icons/Vite.svglApp.astro";
 import ReactIcon from "@src/assets/icons/ReactIcon.svglApp.astro";
 import FontAwesome from "@src/assets/icons/FontAwesome.fontAwesome.astro";
 
 
 const projectsIcons = {
+    astro: IconAstro,
+    w3Schools: W3Schools,
     vite: Vite,
     react: ReactIcon,
     fontAwesome: FontAwesome
