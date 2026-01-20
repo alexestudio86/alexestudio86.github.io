f300310a4c72dbd44587f629457a16ee14827435
Alejandro R <alexestudio86@hotmail.com>
2026-01-20 15:52:32 -0600

    CSS projects section fixed

--- src/components/projects/projectsIcons.js
+++ src/components/projects/projectsIcons.js
@@ -1,16 +1,22 @@
 import AstroLogo from "@src/assets/icons/AstroLogo.svglApp.astro";
-import W3Schools from "@src/assets/icons/W3Schools.astro";
-import Vite from "@src/assets/icons/Vite.svglApp.astro";
-import React from "@src/assets/icons/React.svglApp.astro";
+import CSS from "@src/assets/icons/CSS.svglApp.astro";
 import FontAwesome from "@src/assets/icons/FontAwesome.fontAwesome.astro";
+import JS from "@src/assets/icons/JS.svglApp.astro";
+import HTML from "@src/assets/icons/HTML.svglApp.astro";
+import React from "@src/assets/icons/React.svglApp.astro";
+import Vite from "@src/assets/icons/Vite.svglApp.astro";
+import W3Schools from "@src/assets/icons/W3Schools.astro";
 
 
 const projectsIcons = {
     astro: AstroLogo,
-    w3Schools: W3Schools,
-    vite: Vite,
+    css: CSS,
+    fontAwesome: FontAwesome,
+    js: JS,
+    html: HTML,
     react: React,
-    fontAwesome: FontAwesome
+    vite: Vite,
+    w3Schools: W3Schools
 };
 
 export default projectsIcons;
